9b2cb13ac942c43d885aadc338cc6d7528678a22
emelieribring <emelieribring@hotmail.com>
2023-12-05 19:49:07 +0100

    changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,9 +7,15 @@ export default function Header() {
   return (
     <div className='header'>
         <div className='headerFonts'>
-          <h1 className='headerTitle'>Coin Kings</h1>
-          <h3 className='headerUnderTitle'>Know your Coins?</h3>
-          <p className='headerSubline'>Make Picks. Outperform. Monetise.</p>
+          <div>
+            <h1 className='headerTitle'>Coin Kings</h1>
+          </div>
+          <div>
+            <h3 className='headerUnderTitle'>Know your Coins?</h3>
+          </div>
+          <div>
+            <p className='headerSubline'>Make Picks. Outperform. Monetise.</p>
+          </div>
           <div className="btns">
             <button className='learnBtn'>Play Now</button>
             <Link to="section1" spy={true} smooth={true} duration={500}><button className='learnBtn'>Learn More</button></Link>
